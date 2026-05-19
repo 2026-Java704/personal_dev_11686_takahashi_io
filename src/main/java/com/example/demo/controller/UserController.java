29b372133db1614edd5b1f8a33a2646d22bd5f53
student <student@FJ-1025>
2026-05-19 17:35:02 +0900

    ログイン画面

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,11 +1,48 @@
 package com.example.demo.controller;
 
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.repository.UserRepository;
+
+@Controller
 public class UserController {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+	private final HttpSession session;
+	private final UserRepository userRepository;
 
-		System.out.println("hello");
+	public UserController(
+			HttpSession session,
+			UserRepository userRepository) {
+
+		this.session = session;
+		this.userRepository = userRepository;
 	}
 
+	//ログイン画面表示
+	@GetMapping({ "/", "/login", "/logout" })
+	public String index() {
+		//セッション情報を破棄
+		session.invalidate();
+		return "login";
+
+	}
+
+	//ログインを実行する
+	@PostMapping("/login")
+	public String login(
+			@RequestParam String email,
+			@RequestParam String password,
+			Model model) {
+		/*if (email == null || email.length() == 0) {
+			model.addAttribute("message", "入力して下さい");
+		
+		}*/
+		return "login";
+	}
 }
---
+++ src/main/java/com/example/demo/entity/Recipe.java
@@ -0,0 +1,66 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "recipes")
+public class Recipe {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRecipe() {
+		return recipe;
+	}
+
+	public void setRecipe(String recipe) {
+		this.recipe = recipe;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	private String recipe;
+	@ManyToOne
+	@JoinColumn(name = "category_id")
+	private Category category;
+
+	public Recipe() {
+	}
+
+	public Recipe(Integer id, String name, String recipe, Category category) {
+		this.id = id;
+		this.name = name;
+		this.recipe = recipe;
+		this.category = category;
+	}
+
+}
