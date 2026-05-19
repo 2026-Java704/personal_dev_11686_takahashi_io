1e128449ef124616f7de6b00d39d9fd08bc5eb43
student <student@FJ-1025>
2026-05-19 12:01:54 +0900

    新規作成

---
+++ src/main/java/com/example/demo/PersonalDev11686TakahashiIoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalDev11686TakahashiIoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalDev11686TakahashiIoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.example.demo.controller;
+
+public class UserController {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		System.out.println("hello");
+	}
+
+}
